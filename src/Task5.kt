4a61fd66dc6db22a61093c3f859d9e165449b613
kenm02 <kethptvq@gmail.com>
2024-09-24 11:06:34 +0700

    Update Task5.kt
    clear comment

--- src/Task5.kt
+++ src/Task5.kt
@@ -1,18 +1,17 @@
 fun main() {
-    // Nhập kích thước mảng từ người dùng
+
     print("Nhập kích thước của mảng: ")
     val kichThuoc = readLine()?.toIntOrNull() ?: return
 
-    // Khởi tạo mảng
+
     val mang = IntArray(kichThuoc)
 
-    // Nhập các phần tử của mảng
     for (i in mang.indices) {
         print("Nhập phần tử thứ ${i + 1}: ")
         mang[i] = readLine()?.toIntOrNull() ?: 0
     }
 
-    // Sắp xếp mảng theo thứ tự tăng dần bằng thuật toán nổi bọt
+
     for (i in 0 .. mang.size -1) {
         for (j in 0 until mang.size - 1 ) {
             if (mang[i] < mang[j ]) {
@@ -23,6 +22,6 @@ fun main() {
         }
     }
 
-    // In ra mảng đã sắp xếp
+
     println("Mảng sau khi sắp xếp theo thứ tự tăng dần: ${mang.joinToString(", ")}")
 }
